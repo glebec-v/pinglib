fa9b2986cbb5e22166bac587ca1eaee50d44b4db
glebec-v <gvorotilov@gmail.com>
2018-08-26 01:19:28 +0300

    started tests

---
+++ tests/GlebecV/Model/PingerTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace GlebecV\Model;
+
+use PHPUnit\Framework\TestCase;
+
+class PingerTest extends TestCase
+{
+
+    public function testPing()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testConnect()
+    {
+        $this->assertTrue(true);
+    }
+}
