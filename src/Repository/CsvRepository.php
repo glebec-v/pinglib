6cc2e199cad60e99f62f5e9e6a96cb582b5b712b
glebec-v <gvorotilov@gmail.com>
2018-08-28 01:14:05 +0300

    CsvRepo uses league/csv

--- src/Repository/CsvRepository.php
+++ src/Repository/CsvRepository.php
@@ -2,21 +2,46 @@
 
 namespace GlebecV\Repository;
 
+use GlebecV\Model\Host;
 use GlebecV\RepositoryInterface;
+use League\Csv\Reader;
 
 class CsvRepository implements RepositoryInterface
 {
+    /** @var Reader $reader */
+    private $reader;
 
     /**
      * @return array
+     * @throws \League\Csv\Exception
      */
     public function getHostCollection(): array
     {
-        return [];
+        $ret = [];
+
+        $this->reader->setHeaderOffset(0);
+        $records = $this->reader->getRecords();
+        $header = $this->reader->getHeader();
+        if (isset($header['ip'])) {
+            foreach ($records as $record) {
+                $ret[] = new Host(
+                    $record['ip'],
+                    $record['serial'] ?? null,
+                    $record['name'] ?? null,
+                    $record['vlan'] ?? null
+                );
+            }
+        }
+
+        return $ret;
     }
 
     public function setup(array $config): void
     {
-        // TODO: Implement setup() method.
+        if (!isset($config['file'])) {
+            // todo change to own exception
+            throw new \InvalidArgumentException('Source file is not defined', 1);
+        }
+        $this->reader = Reader::createFromPath($config['file'], 'r');
     }
 }
---
+++ tests/GlebecV/Model/UhpHubPingerTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace GlebecV\Model;
+
+use GlebecV\Model\Ping\UhpHubPinger;
+use PHPUnit\Framework\TestCase;
+
+class UhpHubPingerTest extends TestCase
+{
+
+    public function testPing()
+    {
+        // todo
+        $this->assertTrue(true);
+    }
+
+    public function testConnect()
+    {
+        // todo
+        $this->assertTrue(true);
+    }
+}
